9f361392b73d500eb1edcb152663b0cb6e5863ec
Mayra TS <mayra.be7686@gmail.com>
2023-02-10 21:25:56 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calc-angular';
+
+
+  opA = 0;
+  opB = 0;
+  res = 0;
+ 
+  sumar():void {
+    this.res = this.opA + this.opB;
+  }
+
 }
